668ecf64dceda815f50510160fd642e54e6ed1b5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 21:54:49 -0300

    feat: add entidades, carga DB

---
+++ ms-pedidos/src/main/java/com/github/BeatrizRosa05/ms/pedidos/dto/StatusDTO.java
@@ -0,0 +1,10 @@
+package com.github.BeatrizRosa05.ms.pedidos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class StatusDTO {
+
+}/////////////////////////////
